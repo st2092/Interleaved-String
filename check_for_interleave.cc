aff3fbdaab22b522504669483f6c0f2801d7740c
st2092 <sto002@ucr.edu>
2016-02-06 00:06:47 -0800

    fixed spacing and index

--- check_for_interleave.cc
+++ check_for_interleave.cc
@@ -42,11 +42,11 @@ printTable(bool** table, int row, int col)
  * @return true or false
  */
 bool
-isInterleaved( char* a, char* b, char* c)
+isInterleaved(char* a, char* b, char* c)
 {
-  int len_a = std::strlen(a);
-  int len_b = std::strlen(b);
-  int len_c = std::strlen(c);
+	int len_a = std::strlen(a);
+	int len_b = std::strlen(b);
+	int len_c = std::strlen(c);
 
 	if (len_a + len_b == 0 && len_c == 0)
 	{
@@ -54,54 +54,54 @@ isInterleaved( char* a, char* b, char* c)
 		return true;
 	}
 
-  if (len_a + len_b < len_c)
-  {
-    // string in question is longer than string a and b combine
-    return false;
-  }
+	if (len_a + len_b < len_c)
+	{
+		// string in question is longer than string a and b combine
+		return false;
+	}
 
-  int i, j = 0;
+	int i, j = 0;
 	std::cout << "Creating table ..." << std::endl;
-  // create a memoization table
-  //bool mt[len_a][len_b];
+	// create a memoization table
+	//bool mt[len_a][len_b];
 	bool** mt;
 
-  // initialization of memoization table
+	// initialization of memoization table
 	std::cout << "Starting initialization process..." << std::endl;
 
-  mt = new bool *[len_a + 1];
+    mt = new bool *[len_a + 1];
 	for (i = 0; i <= len_a; i++)
 	{
 		//std::cout << i;
 		mt[i] = new bool [len_b + 1];
 	}
 
-  for (i = 0; i <= len_a;i++)
-  {
-    for (j = 0; j <= len_b; j++ )
+    for (i = 0; i <= len_a;i++)
     {
-      mt[i][j] = false;
-    }
-  }
-
-	// determine if string C is interleaved of A and B utilizing memoization table
-	std::cout << "Determining interleave ..." << std::endl;
-  for(i = 0; i <= len_a; i++)
-  {
-    for(j = 0; j <= len_b; j++)
-    {
-      if (i == 0 && j == 0)
+      for (j = 0; j <= len_b; j++ )
       {
-        // empty strings
-				mt[i][j] = true;
+        mt[i][j] = false;
       }
+    }
+
+	// determine if string C is interleaved of A and B by utilizing memoization table
+	std::cout << "Determining interleave ..." << std::endl;
+	for(i = 0; i <= len_a; i++)
+	{
+		for(j = 0; j <= len_b; j++)
+		{
+			if (i == 0 && j == 0)
+			{
+				// empty strings
+				mt[i][j] = true;
+			}
 
 			if (i == 0 && j >= 1 && c[j-1] == b[j-1])
 			{
 				// string a is empty; compare char in string b and c
 				mt[i][j] = mt[i][j-1];
 			}
-      else if (j == 0 && i >= 1 && c[i-1] == a[i-1])
+			else if (j == 0 && i >= 1 && c[i-1] == a[i-1])
 			{
 				// string b is empty; compare char in string a and c
 				mt[i][j] = mt[i-1][j];
@@ -123,8 +123,8 @@ isInterleaved( char* a, char* b, char* c)
 			{
 				mt[i][j] = mt[i-1][j] || mt[i][j-1];
 			}
-    }
-  }
+		}
+	}
 	if (DEBUG_MODE)
 	{
 		printTable(mt, len_a, len_b);
@@ -143,22 +143,23 @@ int main(int argc, char** argv)
 	}
 	/*
 	char* a = "abc";
-  char* b = "def";
-  char* c = "abcdef";
+	char* b = "def";
+	char* c = "abcdef";
 	*/
 	char* a = argv[1];
 	char* b = argv[2];
 	char* c = argv[3];
 
-  bool is_interleaved = isInterleaved(a, b, c);
+	bool is_interleaved = isInterleaved(a, b, c);
 
 	if (is_interleaved)
 	{
 		std::cout << c << " is interleaved of " << a << " and " << b << std::endl;
-  }
+	}
 	else
 	{
 		std::cout << c << " is not interleaved of " << a << " and " << b << std::endl;
 	}
+	
 	return 0;
 }
